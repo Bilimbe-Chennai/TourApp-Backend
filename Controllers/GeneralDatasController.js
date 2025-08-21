39b240febbf1869ac68400c74b9ef775e92c4145
Bilimbe-Chennai <you@example.com>
2025-08-21 17:54:12 +0530

    general collection api is done

--- Controllers/GeneralDatasController.js
+++ Controllers/GeneralDatasController.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const crudService = require("../Utils/crudService");
 const GeneralDatasCollection = require("../Models/GeneralDatasModel");
 module.exports = {
   save: async (req, res) => {
-    const generalDatasCollection = new GeneralDatasCollection({
-      _id: new mongoose.Types.ObjectId(),
-      dataName: req.body.dataName,
-      datasList: req.body.datasList,
-    });
     try {
-      const result = await generalDatasCollection.save();
+       const result = await crudService.create(GeneralDatasCollection, {
+        ...req.body,
+        _id: new mongoose.Types.ObjectId(),
+      });
       res.status(201).json(result);
     } catch (error) {
       console.error(error);
@@ -17,41 +16,49 @@ module.exports = {
     }
   },
   editgeneralData: async (req, res) => {
-     const { _id, dataName, datasList} = req.body;
-    const generalData = await GeneralDatasCollection.findOneAndUpdate(
-      { dataName: dataName },
-      { $set: {
-          ...(datasList && { datasList }),}},
-          { new: true } // Return the updated document
-    );
-    if (!generalData) return res.status(400).send("Invalid Credentials");
-    res.json({ generalData });
+    try {
+      const { _id, dataName, datasList} = req.body;
+      const generalData = await crudService.updateOne(GeneralDatasCollection, { dataName }, { datasList });
+      if (!generalData) return res.status(400).send("Invalid Data");
+      res.json({ generalData });
+    } catch (error) {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   },
  generalDataGetByName: async (req, res) => {
-    const generalData = await GeneralDatasCollection.findOne({
+    try {
+    const generalData = await crudService.getOne(GeneralDatasCollection,{
       dataName: req.params.dataName,
     });
     if (!generalData)
       return res.status(400).send("Invalid general Data");
     res.json({ generalData });
+    } catch (error) {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   },
   allgeneralDataGet: async (req, res) => {
-    const generalData = await GeneralDatasCollection.find();
+      try {
+    const generalData = await crudService.getAll(GeneralDatasCollection);
     if (!generalData)
       return res.status(400).send("Invalid general Data");
     res.json({ generalData });
+     } catch (error) {
+       console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   },
   deletegeneralDataId: async(req, res) => {
-    const listingQuery = { _id: req.params.id };
+    const _id =  req.params.id ;
     try {
-      const generalData = await GeneralDatasCollection.deleteOne({ _id: req.params.id });
+      const generalData = await crudService.deleteOne(GeneralDatasCollection,{ _id});
       if (generalData) {
         res.status(200).send(`ID ${req.params.id} Deleted Successfully!`);
       } else {
-        res.status(204).json( "data not found");
+        res.status(400).json( "data not found");
       }
     } catch (err) {
-      res.status(204).json( "data not found");
+      res.status(500).json( "data not found");
     }
   },
 };
--- Routes/GeneralRouter.js
+++ Routes/GeneralRouter.js
@@ -3,6 +3,8 @@ const generalDatasController = require("../Controllers/GeneralDatasController");
 const router = express.Router();
 //general datas get router
 router.get(process.env.BASE_GENERALDATACOLLECTION_GETDATABYNAME_URI+"/:dataName", generalDatasController.generalDataGetByName);
-router.post(process.env.BASE_GENERALDATACOLLECTION_CREATEDATABYNAME_URI+"/generaldata", generalDatasController.save);
-router.put(process.env.BASE_GENERALDATACOLLECTION_EDITDATABYNAME_URI+"/generaldata", generalDatasController.editgeneralData);
+router.get(process.env.BASE_GENERALDATACOLLECTION_GETALL_URI, generalDatasController.allgeneralDataGet);
+router.post(process.env.BASE_GENERALDATACOLLECTION_CREATEDATABYNAME_URI, generalDatasController.save);
+router.put(process.env.BASE_GENERALDATACOLLECTION_EDITDATABYNAME_URI, generalDatasController.editgeneralData);
+router.delete(process.env.BASE_GENERALDATACOLLECTION_DELETEDATABYNAME_URI+"/:id", generalDatasController.deletegeneralDataId);
 module.exports = router;
